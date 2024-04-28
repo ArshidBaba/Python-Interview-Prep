fd5e78d89a4639edea2858579eb68111013796d7
Arshid Aslam <arshidbaba93@gmail.com>
2024-04-28 22:17:08 +0530

    Practice

---
+++ iterators.py
@@ -0,0 +1,30 @@
+# mystr = "banana"
+# myit = iter(mystr)
+
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
